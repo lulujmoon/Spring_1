017e89cd9b61e61f09b882a0ce2abd9eb6eb9227
lulujmoon <lulujmoon@gmail.com>
2021-03-19 10:07:26 +0900

    update

--- src/main/java/com/redbeet/s1/member/MemberController.java
+++ src/main/java/com/redbeet/s1/member/MemberController.java
@@ -9,17 +9,17 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class MemberController {
-	
+
 	@Autowired
 	private MemberService memberService;
 	//memberJoin이라는 메서드. print로 확인. url : /member/memberLogin.do
-	
+
 	@RequestMapping(value="member/memberJoin")
 	private String memberJoin() {
 		return "member/memberJoin";
 	}
-	
-	
+
+
 	@RequestMapping(value="member/memberJoin", method= RequestMethod.POST)
 	private void memberJoin2(String id, String pw, String name, String phone, String email) throws Exception {
 		MemberDTO memberDTO = new MemberDTO();
@@ -30,18 +30,18 @@ public class MemberController {
 		memberDTO.setEmail(email);
 		memberService.memberJoin(memberDTO);
 	}
-	
-	
-	
-	
-	
+
+
+
+
+
 	@RequestMapping(value="member/memberLogin") 
 	public String memberLogin(String name, int age) {
 		System.out.println(name);
 		System.out.println(age);
 		return "member/memberLogin";
 	}
-	
+
 	@RequestMapping(value="member/memberLogin", method = RequestMethod.POST)
 	public void memberLogin2(HttpServletRequest request) throws Exception {
 		String id = request.getParameter("id");
@@ -52,7 +52,23 @@ public class MemberController {
 		memberDTO = memberService.memberLogin(memberDTO);
 		System.out.println(memberDTO);
 	}
-	
-	
-	
+
+	//memberLogin2 print
+	@RequestMapping(value = "/member/memberLogin", method = RequestMethod.POST)
+	public String memberLogin(HttpServletRequest request) throws Exception {
+		String id = request.getParameter("id");
+		String pw = request.getParameter("pw");
+
+		MemberDTO memberDTO = new MemberDTO();
+		memberDTO.setId(id);
+		memberDTO.setPw(pw);
+		memberDTO = memberService.memberLogin(memberDTO);
+
+		System.out.println(memberDTO);
+		request.setAttribute("dto", memberDTO);
+		return "member/memberPage";
+
+
+
+	}
 }
