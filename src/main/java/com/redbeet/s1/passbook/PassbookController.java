d2aca1b1b23439b5da020e844e0313f3e0149839
lulujmoon <80494527+lulujmoon@users.noreply.github.com>
2021-03-21 20:55:00 +0900

    class

--- src/main/java/com/redbeet/s1/passbook/PassbookController.java
+++ src/main/java/com/redbeet/s1/passbook/PassbookController.java
@@ -1,15 +1,59 @@
 package com.redbeet.s1.passbook;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class PassbookController {
 	
-	@RequestMapping(value="/passbook/passbookAdd", method=RequestMethod.POST)
-	public String passbookAdd() {
-		return "WEB-INF/views/passbook/passbookAdd.jsp";
+	@Autowired
+	private PassbookService passbookService;
+	
+	@RequestMapping(value="passbook/passbookAdd")
+	public void passbookAdd() {
+		System.out.println("passbookAdd");
+	}
+	
+	@RequestMapping(value="passbook/passbookAdd", method = RequestMethod.POST)
+	public void passbookAdd(String name, double rate, String sell) {
+		
+	}
+	
+	@RequestMapping(value="passbook/passbookList")
+	public void passbookList(Model model) throws Exception {
+		List<PassbookDTO> ar = passbookService.getList();
+		model.addAttribute("list", ar);
+		model.addAttribute("kind", "Passbook");
+	}
+	
+	@RequestMapping(value="passbook/passbookSelect")
+	public void passbookSelect() {
+		
+	}
+	
+	@RequestMapping(value="passbook/passbookUpdate")
+	public void passbookUpdate(){
+		
+	}
+	
+	@RequestMapping(value="passbook/passbookUpdate", method=RequestMethod.POST)
+	public void passbookUpdate(int number){
+		
+	}	
+	
+	@RequestMapping(value="passbook/passbookDelete")
+	public void passbookDelete() {
+		
+	}
+	
+	@RequestMapping(value="passbook/passbookDelete", method=RequestMethod.POST)
+	public void passbookDelete(int number) {
+		
 	}
 
 }
--- src/main/java/com/redbeet/s1/passbook/PassbookDAO.java
+++ src/main/java/com/redbeet/s1/passbook/PassbookDAO.java
@@ -1,9 +1,52 @@
 package com.redbeet.s1.passbook;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class PassbookDAO {
+
+	public Connection connectDB() throws Exception {
+		String user = "user01";
+		String pw = "user01";
+		String url = "jdbc:oracle:thin:@127.0.0.1:1521:xe";
+		String driver = "oracle.jdbc.driver.OracleDriver";
+		Class.forName(driver);
+		Connection con = DriverManager.getConnection(url, user, pw);
+		return con;
+	}
+	
+	public List<PassbookDTO> getList() throws Exception {
+		Connection con = this.connectDB();
+		String sql = "select * from passbook";
+		PreparedStatement ps = con.prepareStatement(sql);
+		ResultSet rs = ps.executeQuery();
+		
+		ArrayList<PassbookDTO> ar = new ArrayList<PassbookDTO>();
+		
+		while(rs.next()) {
+			PassbookDTO passbookDTO = new PassbookDTO();
+			passbookDTO.setNum(rs.getInt("num"));
+			passbookDTO.setName(rs.getString("name"));
+			passbookDTO.setIrate(rs.getDouble("irate"));
+			passbookDTO.setOpenable(rs.getString("openable"));
+			ar.add(passbookDTO);
+		}
+		
+		rs.close();
+		ps.close();
+		con.close();
+		
+		return ar;
+		
+	}
 	
 	
 }
